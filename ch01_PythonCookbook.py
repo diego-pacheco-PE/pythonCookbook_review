f5e4ac42fa50e858317306c802f42587b717db81
yakovsingh <48894356+yakovsingh@users.noreply.github.com>
2021-07-18 23:35:25 -0500

    init2

--- ch01_PythonCookbook.py
+++ ch01_PythonCookbook.py
@@ -52,7 +52,9 @@ print(os.getcwd())
   
 from collections import deque
 print("ex3")
+print('-'*20)
 
+# generator function involving yield. 
 def search(lines, pattern, history=4):
     previous_lines = deque(maxlen = history)
     for line in lines:
@@ -69,4 +71,87 @@ if __name__ == '__main__':
             print('-'*20)
   
 
- 
+q = deque()
+q.append(1)
+q.append(2)
+q.append(3)
+print(q)
+q.appendleft(4)
+print(q)
+q.pop()
+print(q)
+q.popleft()
+print(q)
+
+#  adding or popping items from either end of a queue has O(1) complexity. 
+
+# 1.4 FINDING THE LARGEST OR SMALLEST N ITEMS 
+"""
+You want to make a list of the largest or smallest N items in a collection 
+"""
+import heapq
+
+nums = [1,8,2,2,3,6,-7,323,34,1211,3434,12,232,233]
+print(heapq.nlargest(3,nums))
+print(heapq.nsmallest(4,nums))
+
+portfolio = [
+ {'name': 'IBM', 'shares': 100, 'price': 91.1},
+ {'name': 'AAPL', 'shares': 50, 'price': 543.22},
+ {'name': 'FB', 'shares': 200, 'price': 21.09},
+ {'name': 'HPQ', 'shares': 35, 'price': 31.75},
+ {'name': 'YHOO', 'shares': 45, 'price': 16.35},
+ {'name': 'ACME', 'shares': 75, 'price': 115.65}
+]
+
+cheap = heapq.nsmallest(3, portfolio, key=lambda s: s['price'])
+expensive = heapq.nlargest(1, portfolio, key=lambda s: s['price'])
+
+print(cheap)
+print(expensive)
+
+#order as a heap
+nums = [-2,0,-123,23,2323,54,4,9,-334]
+import heapq
+heap = list(nums)
+#order using heapify! 
+heapq.heapify(heap)
+print(heap)
+heapq.heappop(heap)
+print(heap)
+
+# 1.5 IMPLEMENTING A PRIORITY QUEUE
+"""
+You want to implement a queue that sorts items by a given priority and always returns
+the item with the highest priority on each pop operation. 
+"""
+
+import heapq
+
+class PriorityQueue:
+    def __init__(self):
+        self._queue = []
+        self._index = 0 
+
+    def push(self, item, priority):
+        heapq.heappush(self._queue, (-priority, self._index, item))
+        self._index += 1
+
+    def pop(self):
+        return heapq.heappop(self._queue)[-1]
+
+class Item: 
+    def __init__(self, name):
+        self.name = name
+    def __repr__(self):
+        return 'Item({!r})'.format(self.name)
+
+q = PriorityQueue()
+q.push(Item('foo'),1)
+q.push(Item('bar'),332)
+q.push(Item('spam'),120)
+q.push(Item('grok'),5)
+print(q.pop())
+print(q.pop())
+print(q.pop())
+print(q.pop())
